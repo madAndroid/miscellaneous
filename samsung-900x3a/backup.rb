573dd1368d70eb6a5feb3fee53836a5489c4dc75
Andrew Stangl <andrewstangl@gmail.com>
2013-02-20 08:37:51 +0000

    making progress with backup script

--- samsung-900x3a/backup.rb
+++ samsung-900x3a/backup.rb
@@ -7,9 +7,6 @@ require 'pp'
 
 class OptparseBackup
 
-  CODES = %w[iso-2022-jp shift_jis euc-jp utf8 binary]
-  CODE_ALIASES = { "jis" => "iso-2022-jp", "sjis" => "shift_jis" }
-
   #
   # Return a structure describing the options.
   #
@@ -24,65 +21,33 @@ class OptparseBackup
     options.verbose = false
 
     opts = OptionParser.new do |opts|
-      opts.banner = "Usage: example.rb [options]"
+      opts.banner = "Usage: backup.rb [options]"
 
       opts.separator ""
       opts.separator "Specific options:"
 
-      # Mandatory argument.
-      opts.on("-r", "--require LIBRARY",
-              "Require the LIBRARY before executing your script") do |lib|
-        options.library << lib
+      # source
+      options[:src] = nil
+      opts.on('-s', '--src directory', 'Source directory to backup') do |s|
+        options[:src] = s
       end
 
-      # Optional argument; multi-line description.
-      opts.on("-i", "--inplace [EXTENSION]",
-              "Edit ARGV files in place",
-              "  (make backup if EXTENSION supplied)") do |ext|
-        options.inplace = true
-        options.extension = ext || ''
-        options.extension.sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
+      # source
+      options[:dst] = nil
+      opts.on('-d', '--dst directory', 'Destination directory to backup to') do |d|
+        options[:dst] = d
       end
 
-      # Cast 'delay' argument to a Float.
-      opts.on("--delay N", Float, "Delay N seconds before executing") do |n|
-        options.delay = n
-      end
-
-      # Cast 'time' argument to a Time object.
-      opts.on("-t", "--time [TIME]", Time, "Begin execution at given time") do |time|
-        options.time = time
-      end
-
-      # Cast to octal integer.
-      opts.on("-F", "--irs [OCTAL]", OptionParser::OctalInteger,
-              "Specify record separator (default \\0)") do |rs|
-        options.record_separator = rs
-      end
-
-      # List of arguments.
-      opts.on("--list x,y,z", Array, "Example 'list' of arguments") do |list|
-        options.list = list
-      end
-
-      # Keyword completion.  We are specifying a specific set of arguments (CODES
-      # and CODE_ALIASES - notice the latter is a Hash), and the user may provide
-      # the shortest unambiguous text.
-      code_list = (CODE_ALIASES.keys + CODES).join(',')
-      opts.on("--code CODE", CODES, CODE_ALIASES, "Select encoding",
-              "  (#{code_list})") do |encoding|
-        options.encoding = encoding
-      end
-
-      # Optional argument with keyword completion.
-      opts.on("--type [TYPE]", [:text, :binary, :auto],
-              "Select transfer type (text, binary, auto)") do |t|
-        options.transfer_type = t
+      # source
+      options[:exclude] = nil
+      opts.on('-e', '--exclude glob', 'Any patterns to exclude') do |e|
+        options[:exclude] = e
       end
 
       # Boolean switch.
+      options[:verbose] = nil
       opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-        options.verbose = v
+        options[:verbose] = v
       end
 
       opts.separator ""
@@ -108,11 +73,17 @@ class OptparseBackup
 
 end  # class OptparseBackup
 
-
 options = OptparseBackup.parse(ARGV)
 pp options
 
 class Backup
 
-
+  def initialize(source, destination, excludes)
+    @source = source
+    @destination = destination
+    @excludes = excludes
+  end
+  
 end
+
+
